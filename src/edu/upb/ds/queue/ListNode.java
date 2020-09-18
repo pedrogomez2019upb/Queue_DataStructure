1a5d192e03ce4a9b89a3fead27c40cea5109fe5b
Pedro Felipe Gomez <pedro.gomez.2019@upb.edu.co>
2020-09-18 16:11:10 -0500

    Initial commit

---
+++ src/edu/upb/ds/queue/ListNode.java
@@ -0,0 +1,51 @@
+package edu.upb.ds.queue;
+
+public class ListNode {
+    public Object object;
+    public ListNode next;
+
+    public ListNode(Object object)
+    {
+        this.object = object;
+        this.next = null;
+    }
+    public ListNode(){}
+
+    public ListNode(Object object, ListNode next) {
+        this.object = object;
+        this.next = next;
+    }
+
+    public Object getObject()
+    {
+        return object;
+    }
+
+    public void setObject(Object object)
+    {
+        this.object = object;
+    }
+
+    public void linkNext(ListNode n)
+    {
+        next=n;
+    }
+    public ListNode getNext()
+    {
+        return next;
+    }
+
+    public void setNext(ListNode next)
+    {
+        this.next = next;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ListNode{" +
+                "object=" + object +
+                ", next=" + next +
+                '}';
+    }
+}
